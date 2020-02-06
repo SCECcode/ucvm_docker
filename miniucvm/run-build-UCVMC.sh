85c092d2330c520a88265db5f6b227b768e5a456
Mei-Hui Su <mei@MeiPro.local>
2020-02-06 00:26:39 -0800

    wrong syntax

--- miniucvm/run-build-UCVMC.sh
+++ miniucvm/run-build-UCVMC.sh
@@ -24,7 +24,8 @@ cd $UCVM_SRC_PATH
 $UCVM_INSTALL_PATH
 EOF
 
-if [ -e $UCVM_SRC_PATH/bash_ucvm.sh ] then
+if [ -e $UCVM_SRC_PATH/bash_ucvm.sh ] 
+then
   cp $UCVM_SRC_PATH/bash_ucvm.sh $UCVM_INSTALL_PATH/config
 fi
 
