30385df7be790466ff70118b0793108b8f92309f
Philip Maechling <pjmaechling@users.noreply.github.com>
2021-09-13 14:10:39 -0700

    tag and push all ucvm docker images to dockerhub

--- ucvm_v21_10/tag_one.sh
+++ ucvm_v21_10/tag_one.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
-docker tag ucvm_1210_cvms5:09121132 sceccode/ucvm_1210_cvms5:09121132
-docker push sceccode/ucvm_1210_cvms5:09121132
+docker tag ucvm_1210_cvms5:09131139 sceccode/ucvm_1210_cvms5:09121139
+docker push sceccode/ucvm_1210_cvms5:09131139
 #
-docker tag ucvm_1210_cvmsi:09121132 sceccode/ucvm_1210_cvmsi:09121132
-docker push sceccode/ucvm_1210_cvmsi:09121132
+docker tag ucvm_1210_cvmsi:09131139 sceccode/ucvm_1210_cvmsi:09131139
+docker push sceccode/ucvm_1210_cvmsi:09131139
 #
-docker tag ucvm_1210_cvmh:09121132 sceccode/ucvm_1210_cvmh:09121132
-docker push sceccode/ucvm_1210_cvmh:09121132
+docker tag ucvm_1210_cvmh:09131139 sceccode/ucvm_1210_cvmh:09131139
+docker push sceccode/ucvm_1210_cvmh:09131139
 #
-docker tag ucvm_1210_cvms:09121132 sceccode/ucvm_1210_cvms:09121132
-docker push sceccode/ucvm_1210_cvms:09121132
+docker tag ucvm_1210_cvms:09131139 sceccode/ucvm_1210_cvms:09131139
+docker push sceccode/ucvm_1210_cvms:09131139
 #
-docker tag ucvm_1210_ivlsu:09121132 sceccode/ucvm_1210_ivlsu:09121132
-docker push sceccode/ucvm_1210_ivlsu:09121132
+docker tag ucvm_1210_ivlsu:09131139 sceccode/ucvm_1210_ivlsu:09131139
+docker push sceccode/ucvm_1210_ivlsu:09131139
 #
-docker tag ucvm_1210_cvlsu:09121132 sceccode/ucvm_1210_cvlsu:09121132
-docker push sceccode/ucvm_1210_cvlsu:09121132
+docker tag ucvm_1210_cvlsu:09131139 sceccode/ucvm_1210_cvlsu:09131139
+docker push sceccode/ucvm_1210_cvlsu:09131139
 #
-docker tag ucvm_1210_cca:09121132 sceccode/ucvm_1210_cca:09121132
-docker push sceccode/ucvm_1210_cca:09121132
+docker tag ucvm_1210_cca:09131139 sceccode/ucvm_1210_cca:09131139
+docker push sceccode/ucvm_1210_cca:09131139
 #
-docker tag ucvm_1210_albacore:09121132 sceccode/ucvm_1210_albacore:09121132
-docker push sceccode/ucvm_1210_albacore:09121132
+docker tag ucvm_1210_albacore:09131139 sceccode/ucvm_1210_albacore:09131139
+docker push sceccode/ucvm_1210_albacore:09131139
 #
-docker tag ucvm_1210_wfcvm:09121132 sceccode/ucvm_1210_wfcvm:09121132
-docker push sceccode/ucvm_1210_wfcvm:09121132
+docker tag ucvm_1210_wfcvm:09131139 sceccode/ucvm_1210_wfcvm:09131139
+docker push sceccode/ucvm_1210_wfcvm:09131139
 #
-docker tag ucvm_1210_cencal:09121132 sceccode/ucvm_1210_cencal:09121132
-docker push sceccode/ucvm_1210_cencal:09121132
+docker tag ucvm_1210_cencal:09131139 sceccode/ucvm_1210_cencal:09131139
+docker push sceccode/ucvm_1210_cencal:09131139
