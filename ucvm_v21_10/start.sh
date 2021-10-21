6064b5f9cac69c3f3dc3558d3ea79aa01b183d01
Philip Maechling <pjmaechling@users.noreply.github.com>
2021-10-20 22:05:02 -0700

    added dockerimage with full mpicc stack included

--- ucvm_v21_10/start.sh
+++ ucvm_v21_10/start.sh
@@ -1,1 +1,1 @@
-docker run --rm -it --mount type=bind,source="$(pwd)"/target,destination=/app/target  ucvm_1210_cvms5:09131731
+docker run --rm -it --mount type=bind,source="$(pwd)"/target,destination=/app/target  ucvm_1210_albacore:09131731
