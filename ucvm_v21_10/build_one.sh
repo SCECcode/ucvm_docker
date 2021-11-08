59229c88e3c5c7063bed59952304c1f80ce056c0
Philip Maechling <pjmaechling@users.noreply.github.com>
2021-11-08 07:23:50 -0800

    updated Dockefile for v21_10 release w/o python2

--- ucvm_v21_10/build_one.sh
+++ ucvm_v21_10/build_one.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-cp ./models/cvms5.tar.gz ./ucvm/largefiles/.
+cp ../../ucvm/largefiles/cvms5.tar.gz ./ucvm/largefiles/.
 docker build . -f Dockerfile --no-cache=true -t ucvm_1210_cvms5:09122303
 rm ./ucvm/largefiles/cvms5.tar.gz 
---
+++ ucvm_v21_10/run_one.sh
@@ -0,0 +1,2 @@
+docker run --rm -it --mount type=bind,source="$(pwd)"/target,destination=/app/target  ucvm_1210_cvms5:09122303
+
