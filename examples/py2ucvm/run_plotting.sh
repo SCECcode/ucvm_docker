4a01ee2008587844435a68ec7cff93cfac3f9c5a
Mei-Hui Su <mei@MeiPro.local>
2021-12-06 11:57:16 -0800

    local changes

--- examples/py2ucvm/run_plotting.sh
+++ examples/py2ucvm/run_plotting.sh
@@ -9,5 +9,5 @@ docker run -e DISPLAY=host.docker.internal:0 --rm -it --mount type=bind,source="
 
 ## once your container runs
 ##
-## plot_horizontal_slice.py -b 33.35,-118 -u 34.35,-117 -e 1000 -d vs -c cvmsi -a s -s 0.01 -o new.png -i /usr/local/opt/ucvm
+## plot_horizontal_slice.py -b 33.35,-118 -u 34.35,-117 -e 1000 -d vs -c cvmh -a s -s 0.01 -o new.png -i /usr/local/opt/ucvm
 ## view_png.py -f new.png
